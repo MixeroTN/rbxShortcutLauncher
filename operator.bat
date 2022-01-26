0fecd258aea620560db817e1be2b7a8aa5cb127a
MixeroTN <40803091+michalll000@users.noreply.github.com>
2022-01-26 20:05:24 +0100

    Update operator.bat

--- operator.bat
+++ operator.bat
@@ -3,7 +3,7 @@ set arg1=%1
 taskkill /IM "firefox.exe" /f
 start "" "firefox.exe" -headless "https://www.roblox.com/home?placeId=%arg1%&dontClose=1"
 :search
-tasklist|find "RobloxPlayerBeta"
+tasklist|find "RobloxPlayerLauncher.exe"
 IF %ERRORLEVEL% == 0 GOTO :found
 TIMEOUT /T 5
 GOTO :search
